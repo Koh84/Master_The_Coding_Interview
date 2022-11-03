12b05e16bf2ff2d7378d876d3645cabd3c64f793
Kelvin <koh84@hotmail.com>
2022-11-03 21:51:12 +0000

    add big O notation of O(n) linear time

---
+++ 1_BIG_O/linear/main.js
@@ -0,0 +1,27 @@
+const { performance } = require('perf_hooks');
+
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin',
+    'nemo', 'gill', 'bloat',
+    'nigel', 'squirt', 'darla',
+    'hank'];
+const large = new Array(100000).fill('nemo');
+
+function findNemo(array) {
+    let t0 = performance.now()
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log('Found NEMO!');
+        }
+    }
+    let t1 = performance.now()
+    console.log(`Call to find Nemo took ${t1 - t0} milliseconds`)
+}
+
+
+findNemo(large);
+//Big O notation of O(n) --> Linear Time
+//n is the input element size
+//4 elements array - 4 loops
+//5 elements array - 5 loops
+//n elements array - n loops
